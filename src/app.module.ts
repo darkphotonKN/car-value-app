c103e5a85128c1bb6edc7437cf8c28949f52da28
Kranti <darkphoton20@gmail.com>
2022-05-29 18:31:39 +0800

    initial app structure.

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, UsersController],
+  providers: [UsersService],
+})
+export class AppModule {}
